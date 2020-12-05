679cd47c519a657aca64c3a99a14e83b8c4ecee6
alessia-besson <alessia.besson@gmail.com>
2020-12-05 16:23:04 +0100

    Adapt speed settings

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,70 @@
-/*!
-    * Start Bootstrap - Resume v6.0.2 (https://startbootstrap.com/theme/resume)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-    */
-    (function ($) {
-    "use strict"; // Start of use strict
-
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-        if (
-            location.pathname.replace(/^\//, "") ==
-                this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
-        ) {
-            var target = $(this.hash);
-            target = target.length
-                ? target
-                : $("[name=" + this.hash.slice(1) + "]");
-            if (target.length) {
-                $("html, body").animate(
-                    {
-                        scrollTop: target.offset().top,
-                    },
-                    1000,
-                    "easeInOutExpo"
-                );
-                return false;
-            }
-        }
-    });
-
-    // Closes responsive menu when a scroll trigger link is clicked
-    $(".js-scroll-trigger").click(function () {
-        $(".navbar-collapse").collapse("hide");
-    });
-
-    // Activate scrollspy to add active class to navbar items on scroll
-    $("body").scrollspy({
-        target: "#sideNav",
-    });
-})(jQuery); // End of use strict
+window.formbutton =
+  window.formbutton ||
+  function() {
+    (formbutton.q = formbutton.q || []).push(arguments);
+  };
+formbutton("create", {
+  action: "https://formspree.io/f/mjvpeqll",
+  title: "Contact Me",
+  fields: [
+    {
+      type: "text",
+      label: "Name:",
+      name: "name",
+    },
+    {
+      type: "email",
+      label: "Email:",
+      name: "email",
+    },
+    {
+      type: "textarea",
+      label: "Message:",
+      name: "message",
+    },
+    { type: "submit", value: "Send" }
+  ],
+  styles: {
+    fontFamily: "Muli",
+    modal: {
+      border: "1px solid #6D6875",
+      borderRadius: "0",
+    },
+    title: {
+      padding: "24px 24px 0px 24px",
+      background: "rgba(0,0,0,0)",
+      color: "#2e2a37",
+      fontFamily: "Muli",
+      fontSize: "2em",
+    },
+    body: {
+        padding: "16px 24px 24px",
+    },
+    field: {
+      display: "flex",
+    },
+    submitField: {
+      justifyContent: "flex-end",
+    },
+    label: {
+      width: "40%",
+    },
+    checkboxLabel: {
+      width: "auto",
+    },
+    input: {
+      borderRight: "1px solid rgba(0,0,0,0.1)",
+      borderBottom: "1px solid rgba(0,0,0,0.1)",
+      borderRadius: "0px"
+    },
+    button: {
+      background: "white",
+      fill: "#6D6875",
+      border: "1px solid #6D6875",
+    },
+    closeButton: {
+      textShadow: "0 0 0 #2e2a37",
+    }
+  },
+  initiallyVisible: true
+});
